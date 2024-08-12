cd90e194a86ebfe15f41b530ddea1535200df46d
Gustavo <gustavo.estudo017@gmail.com>
2024-08-12 10:16:16 -0300

    aula 1 - completo

---
+++ Aula 01 - 12-08-24/aula1/Aluno.java
@@ -0,0 +1,17 @@
+package aula1;
+
+public class Aluno {
+	
+	String nome;
+	short idade;
+	String genero;
+	String matricula;
+	
+	void estudar() {
+		System.out.println("vai estudar moleque");
+	}
+	
+	void comer() {
+		System.out.println("nao se esquece de comer");
+	}
+}
---
+++ Aula 01 - 12-08-24/aula1/Mesa.java
@@ -0,0 +1,15 @@
+package aula1;
+
+public class Mesa {
+	String material;
+	short pernas;
+	String formato;
+	
+	void empurar() {
+		System.out.println("empura a mesa de volta para a sala");
+	}
+	
+	void colocar() {
+		System.out.println("coloque os talheres sobre a mesa");
+	}
+}
